c6fe69f3766c6d55b1c80d28e669528c11892ccd
Md Nasim Khan <nasim15-4211@diu.edu.bd>
2024-09-14 20:10:09 +0600

    Method Implementation

--- app/src/main/java/com/example/thenewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/thenewsapp/ui/NewsViewModel.kt
@@ -1,12 +1,21 @@
 package com.example.thenewsapp.ui
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.thenewsapp.models.Article
 import com.example.thenewsapp.models.NewsResponse
 import com.example.thenewsapp.repository.NewsRepository
 import com.example.thenewsapp.util.Resource
+import kotlinx.coroutines.launch
 import retrofit2.Response
+
 
 class NewsViewModel(app:Application,val newsRepository: NewsRepository):AndroidViewModel(app) {
     var headlines:MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
@@ -20,23 +29,65 @@ class NewsViewModel(app:Application,val newsRepository: NewsRepository):AndroidV
 
     private fun handleHeadlineResponse(response: Response<NewsResponse>):Resource<NewsResponse> {
         if (response.isSuccessful){
-            response.body().let { resultResponse ->
+            response.body()?.let { resultResponse ->
                 headlinesPage++
-            if (headlinesResponse == null)
-            {
+            if (headlinesResponse == null) {
                 headlinesResponse = resultResponse
             }
-            else
-            {
-//                val oldArticles = headlinesResponse?.articles
-//                val newArticles = resultResponse.articles
-//                oldArticles?.addAll(newArticles)
+            else {
+                val oldArticles = headlinesResponse?.articles
+                val newArticles = resultResponse.articles
+                oldArticles?.addAll(newArticles)
             }
-                //return Resource.Success(headlinesResponse ?: resultResponse)
+                return Resource.Success(headlinesResponse?: resultResponse)
             }
         }
         return  Resource.Error(response.message())
 
     }
+private  fun headleSearchNewsResponse(response: Response<NewsResponse>):Resource<NewsResponse>{
+    if (response.isSuccessful){
+        response.body()?.let { resultResponse ->
+            if (searchNewsResponse == null || newSearchQuery != oldSearchQuery)
+            {
+                searchNewsPage=1
+                oldSearchQuery=newSearchQuery
+                searchNewsResponse = resultResponse
+            }
+            else
+            {
+                searchNewsPage++
+                val oldArticles = searchNewsResponse?.articles
+                val newArticles = resultResponse.articles
+                oldArticles?.addAll(newArticles)
+            }
+            return Resource.Success(searchNewsResponse ?: resultResponse)
+        }
+    }
+    return  Resource.Error(response.message())
 
+    }
+    fun addToFavourites(article: Article)=viewModelScope.launch{
+        newsRepository.upsert(article)
+    }
+    fun getFavouriteNews()=newsRepository.getFavouriteNews()
+
+    fun deleteArticle(article: Article)=viewModelScope.launch{
+        newsRepository.deleteArticle(article)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun internetConnection(context: Context): Boolean{
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).apply {
+            return getNetworkCapabilities(activeNetwork)?.run {
+                when{
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI)->true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)->true
+                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)->true
+                    else ->false
+                }
+            }?:false
+        }
+    }
 }
+
